ca39bfb991e75c862645e87f4f619587619ff002
ConstantineWang <constantinewang@ucla.edu>
2023-08-02 21:27:07 -0700

    final v1

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
+AOS.init();
+
 var width = $(window).width(); 
 window.onscroll = function(){
 if ((width >= 1000)){
     if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#header").css("background","#fff");
-        $("#header").css("color","black");
-        $(".about-header").css("color","black");
-        $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
-        $("#header").css("padding","4vh 4vw");
+        
+        $("#header").css({
+            "background": "rgba(255,255,255,0.75)", // semi-transparent background
+            "color": "black",
+            "box-shadow": "0px 3px 10px rgba(0,0,0,0.1)",
+            "padding": "4vh 4vw",
+            "border-radius": "15px",
+            "backdrop-filter": "blur(10px)",
+            "margin-top": "20px",
+            "width": "90%",
+            "margin-left": "5%",
+            "margin-right": "5%",
+            "padding":"2vh",
+
+            // blurring the background
+        });
         $("#navigation a").hover(function(){
             $(this).css("border-bottom","2px solid rgb(255, 44, 90)");
         },function(){
             $(this).css("border-bottom","2px solid transparent");
         });
     }else{
-        
+        $("#header").css("backdrop-filter","");
+        $("#header").css("width","");
+        $("#header").css("margin","0");
         $("#header").css("background","transparent");
         $("#header").css("color","black");
         $(".about-header").css("color","white");
         $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
         $("#header").css("padding","6vh 4vw");
+        $("#header").css("")
         $("#navigation a").hover(function(){
             $(this).css("border-bottom","2px solid #fff");
         },function(){
